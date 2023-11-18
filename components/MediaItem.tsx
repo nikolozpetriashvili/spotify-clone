c612d3be4372e28f00323409d54e4474941db1d7
shirase <nikushapetriashvili@gmail.com>
2023-11-19 00:10:39 +0400

    update media item

--- components/MediaItem.tsx
+++ components/MediaItem.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import { Song } from "@/types";
 import useLoadImage from "@/hooks/useLoadImage";
 import Image from "next/image";
+import usePlayer from "@/hooks/usePlayer";
 
 interface MediaItemProps {
   data:Song;
@@ -12,16 +13,17 @@ interface MediaItemProps {
 
 const MediaItem: React.FC<MediaItemProps> = ({data,onClick}) => {
   const imageUrl = useLoadImage(data);
+  const player = usePlayer();
   const handleClick = () => {
     if(onClick) {
       return onClick(data.id);
     }
 
-    //turn on player
+    return player.setId(data.id);
   }
 
   return (
-    <div className="flex items-center gap-x-3 cursor-pointer hover:bg-neutral-800/50 w-full p-2 rounded-md">
+    <div onClick={handleClick} className="flex items-center gap-x-3 cursor-pointer hover:bg-neutral-800/50 w-full p-2 rounded-md">
       <div className="relative rounded-md min-h-[48px] min-w-[48px] overflow-hidden">
         <Image fill src={imageUrl || '/images/liked.png'} alt="Media Item" className="object-cover"/>
       </div>
