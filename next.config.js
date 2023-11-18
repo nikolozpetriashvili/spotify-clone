ba85c4cbd1656a62bde70f21d14715da21532c49
shirase <nikushapetriashvili@gmail.com>
2023-11-19 02:23:38 +0400

    next.config

--- next.config.js
+++ next.config.js
@@ -7,17 +7,4 @@ const nextConfig = {
   }
 }
 
-module.exports = {
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: 'https://spotify-clone-nine-flame.vercel.app/',
-        permanent: false, // set to true for permanent redirect (301), false for temporary redirect (302)
-      },
-      // Add more redirects for other routes if needed
-    ];
-  },
-};
-
 module.exports = nextConfig
